8c9edc38f5d4880ad7ba29b21ab94c6bebdf1b75
Carlos Banks <48330194+carlostbanks@users.noreply.github.com>
2023-07-04 01:10:18 +0000

    done

--- src/app.py
+++ src/app.py
@@ -103,6 +103,9 @@ def user_login():
     return jsonify({"response": "Successfully logged in", "token": token, "email": user.email}), 200
 
 
+
+
+
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3001))
